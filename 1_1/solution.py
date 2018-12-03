d5198a8cead1014757d36c5d461475005919c3a2
Daniel Zuerrer <daniel.zuerrer@yahoo.de>
2018-12-03 10:38:46 +0100

    made easier

--- 1_1/solution.py
+++ 1_1/solution.py
@@ -1,19 +1,8 @@
-from functools import reduce
-
-def addOrSubtract(value, operation):
-    if operation[0] == '+':
-        return value + int(operation[1])
-    elif operation[0] == '-':
-        return value - int(operation[1])
-
-
 with open('input.txt') as input:
     frequencyChanges = input.readlines()
 
-frequencyChanges = [change.strip() for change in frequencyChanges]
+frequencyChanges = [int(change.strip()) for change in frequencyChanges]
 
-frequencyOperations = [(change[0], change[1:]) for change in frequencyChanges]
-
-frequency = reduce(lambda value, operation: addOrSubtract(value, operation), frequencyOperations, 0)
+frequency = sum(frequencyChanges, 0)
 
 print(frequency)
--- 1_2/solution.py
+++ 1_2/solution.py
@@ -1,26 +1,15 @@
-from functools import reduce
-
-def addOrSubtract(value, operation):
-    if operation[0] == '+':
-        return value + int(operation[1])
-    elif operation[0] == '-':
-        return value - int(operation[1])
-
-
 with open('input.txt') as input:
     frequencyChanges = input.readlines()
 
-frequencyChanges = [change.strip() for change in frequencyChanges]
-
-frequencyOperations = [(change[0], change[1:]) for change in frequencyChanges]
+frequencyChanges = [int(change.strip()) for change in frequencyChanges]
 
 frequency = 0
 knownFrequencies = [frequency]
 searching = True
 
 while searching:
-    for frequencyOperation in frequencyOperations:
-        frequency = addOrSubtract(frequency, frequencyOperation)
+    for frequencyChange in frequencyChanges:
+        frequency += frequencyChange
         if frequency in knownFrequencies:
             searching = False
             break
